59aa1bcc0e2c12551ebab1a6fc9fc79c67d1b2bd
TomaszRadwanski <t.l.radwanski@gmail.com>
2017-04-04 12:48:09 +0100

    version4 working click on falling animations

--- js/app.js
+++ js/app.js
@@ -1,57 +1,54 @@
 $(init);
-
-const width        = 4;
-const height       = 5;
-const falling      = [];
-let numberOfBlocks = 0;
-let score          = 0;
-let interval     = 1000;
-let $body;
-let loop;
+let $main;
 let $board;
+let interval = 3000;
+let falling = [];
 
-function init() {
+function init (){
   createBoard();
   $('button').on('click', startGame);
-}
-
-function createBoard() {
-  $body  = $('body');
-  $board = $('<ul class="board"></ul>');
-  $body.append($board);
-  for (let i = 0; i < width*height; i++) {
-    $board.append('<li></li>');
-  }
-}
-
-function startGame() {
-  setInterval(shiftGrid, interval);
   $('.board').on('click', '.falling', removeFalling);
 }
 
-function shiftGrid() {
-  for (let i = 0; i < width; i++) $board.prepend('<li></li>');
-  $('li:nth-last-child(-n+4)').remove();
-  selectFalling();
+function startGame(){
+  setInterval(createFallingDiv, interval/4.7);
 }
 
-function selectFalling() {
-  const first4 = $('li:lt(4)');
-  const item = first4[Math.floor(Math.random()*first4.length)];
-  $(item).addClass(numberOfBlocks.toString());
-  $(item).addClass('falling');
-  falling.push(numberOfBlocks);
-  numberOfBlocks++;
+function createFallingDiv (){
+  const compSelection = $('.col')[Math.floor(Math.random()*$('.col').length)];
+  const $newDiv = $('<div>', { class: 'falling'});
+  $(compSelection).append($newDiv);
+  $newDiv.animate({
+    bottom: 0
+  }, {
+    duration: interval,
+    easing: 'linear',
+    complete: function() {
+      $('p').text('hit');
+      $(this).remove();
+    }
+  });
+
+  // Push DOM object into the array
+  falling.push($newDiv[0]);
 }
 
-function removeFalling() {
-  const fallingIndex = parseInt($(this).attr('class'));
-  if (falling[0] === fallingIndex) {
-    $(this).removeClass('falling');
+function removeFalling(e) {
+  // Can't make comparison as a jQuery object?
+  if (falling.indexOf(e.target) === 0) {
     falling.shift();
-    interval-=100;
-    console.log(interval);
-    score ++;
-    $('p').text(`Score: ${score}`);
+    $(this).stop(true).remove();
+    // $(this).fadeOut('slow', function() {
+    //   $(this).remove();
+    // });
+  }
+}
+
+function createBoard() {
+  $main  = $('main');
+  $board = $('<div class="board"></div>');
+  $main.append($board);
+  for (let i = 0; i < 4; i++) {
+    $board.append('<div class="col"></div>');
   }
 }
